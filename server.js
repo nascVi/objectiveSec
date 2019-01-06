4cd11fe5c5b0499511ae110525aa286e6942e949
Vaiiho Nascimbene <vaiiho.nascimbene@gmail.com>
2019-01-06 22:19:33 +0100

    new file:   .gitignore 	new file:   objectives.db 	new file:   package-lock.json 	modified:   package.json 	modified:   server.js 	new file:   yarn.lock

--- server.js
+++ server.js
@@ -1,5 +1,50 @@
-//
-//
-console.log('this is node running on android');
-//const appollo = require("appollo");
+// server.js
 
+// DEPENDENCIES AND SETUP
+// ===============================================
+
+var express = require('express'),
+  app = express(),
+  port = Number(process.env.PORT || 8080);
+
+// DATABASE
+// ===============================================
+
+// Setup the database.
+var Datastore = require('nedb');
+var db = new Datastore({
+  filename: 'objectives.db', /* provide a path to the 
+database 
+file*/ 
+  autoload: true, // automatically load the database
+  timestampData: true /* automatically add and manage 
+the 
+fields createdAt and updatedAt*/
+});
+
+// Check one can save to the database.
+// name an objective.
+var objective = {
+  description: 'Do 10 minutes for a nap every day',
+};
+
+// Save this goal to the database.
+db.insert(objective, function(err, newObjective) {
+  if (err) console.log(err);
+  console.log(newObjective);
+});
+
+// ROUTES
+// ===============================================
+
+// Define the home page route.
+app.get('/', function(req, res) {
+  res.send('Our first route is working. :)');
+});
+
+// START THE SERVER
+// ===============================================
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
